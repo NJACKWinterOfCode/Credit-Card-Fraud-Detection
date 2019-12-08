5bf0d90a3739f235ba5fa0299c75fb2ce1c4e8db
saturn660 <saurabhk660@gmail.com>
2019-12-08 18:44:43 +0530

    added notebook with random forest model( quick for testing) and a list of features ranked by importance

--- random_forest.py
+++ random_forest.py
@@ -1,8 +1,40 @@
 # Importing essential libraries
 import pandas as pd
 import numpy as np 
+import matplotlib.pyplot as plt
+from sklearn.preprocessing import MinMaxScaler
+from skewedmetrics import precision
+from sklearn import linear_model
 
 # Data visualisation
 data = pd.read_csv("Dataset/creditcard.csv")
+data = data.dropna( axis =0)
+X = data.loc[:,'Time':'Amount']
+y = data['Class']
 
-print(data.head())
+clf = linear_model.SGDClassifier(max_iter=1000, tol= 1e-3)
+clf.fit( X,y)
+ytrue = y == 1
+#print(ytrue)
+ypred = clf.predict(X[ytrue])
+
+count = (y[ytrue] == ypred).value_counts()
+print(count)
+print( "ans is %f" %( precision( y[ytrue], ypred) ))
+
+'''ndata = data[data['Class'] == 0]
+pdata = data[data['Class'] == 1]
+print( ndata['Class'].value_counts())
+print( pdata['Class'].value_counts())
+
+scaler =MinMaxScaler((-1,1))
+for item in data.columns:
+    if data[item].max() >3 or data[item].min() <-3:
+        data[[item]] = scaler.fit_transform(data[[item]])
+    print( "%s, max = %f, min = %f"%(item, data[item].max(), data[item].min() ))
+
+
+plt.hist( pdata['V5'] , bins = 50,edgecolor = 'blue', log = True)
+plt.xlabel('V5')
+plt.ylabel('Number')'''
+plt.show()
